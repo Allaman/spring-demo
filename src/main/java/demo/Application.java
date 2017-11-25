08edd55527830f1e3bcb11d9b0c3ce122ceb24b3
Michael <michaeljohannpeter@gmail.com>
2017-11-25 20:07:47 +0100

    added yaml config external configuration

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,37 +1,28 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
+
+    // Simple Environment-Value
+    // Can be overwritten in docker-compose
     @Value("${foo:default_value}")
-    private String awesome;
+    private String foo;
 
-    @ConfigurationProperties(prefix="bar")
-    public class barProperties {
+    @Autowired
+    private ApplicationProperties applicationProperties;
 
-        private String firstName;
-
-        public String getFirstName() {
-            return this.firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-    }
 
     @RequestMapping("/")
     public String home() {
-        barProperties b = new barProperties();
-        return "Hello " + awesome + " " + b.getFirstName();
+        return "Hello " + foo + "<br>" + applicationProperties;
     }
 
     public static void main(String[] args) {
