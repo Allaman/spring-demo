a971e724025636e1f55712ac346aaec6ea6595c4
Allaman <michaeljohannpeter@gmail.com>
2017-12-02 21:06:57 +0100

    implemented Lombok

--- src/main/java/demo/YamlProperties.java
+++ src/main/java/demo/YamlProperties.java
@@ -6,63 +6,21 @@ import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString(includeFieldNames = false)
 @Component
 @ConfigurationProperties("server")
 public class YamlProperties {
-
     private String email;
     private List<Cluster> cluster = new ArrayList<>();
 
+    @Data
+    @ToString(includeFieldNames = false)
     public static class Cluster {
         private String ip;
         private String path;
-
-        public String getIp() {
-            return ip;
-        }
-
-        public void setIp(String ip) {
-            this.ip = ip;
-        }
-
-        public String getPath() {
-            return path;
-        }
-
-        public void setPath(String path) {
-            this.path = path;
-        }
-
-        @Override
-        public String toString() {
-            return "Cluster{" +
-                    "ip='" + ip + '\'' +
-                    ", path='" + path + '\'' +
-                    '}';
-        }
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Cluster> getCluster() {
-        return cluster;
-    }
-
-    public void setCluster(List<Cluster> cluster) {
-        this.cluster = cluster;
-    }
-
-    @Override
-    public String toString() {
-        return "YamlProperties{" +
-                "email='" + email + '\'' +
-                ", cluster=" + cluster +
-                '}';
     }
 }
